322a9a75c24b8159dd39eed997619c68d324bf4a
K9Design <dog@k9design.co.uk>
2021-12-01 12:09:00 +0000

    fetching and displaying first movie title on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    getMovieData();
+  }, [])
+
+  const getMovieData = async () => {
+    console.log("run once");
+    const results = await axios.get("https://ghibliapi.herokuapp.com/films");
+    console.log("response: ", results);
+    setMovieData(results.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ghibli Movie</h1>
+      <div className="data-ui"><h3>data: {movieData.length > 0 ? movieData[0].title : "waiting for data..."}</h3></div>
     </div>
   );
 }
