4e62c4ff67566cfbd6e15d8a76cfac407b9f956f
K9Design <dog@k9design.co.uk>
2021-12-01 15:17:32 +0000

    separated work into component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import react, { useState, useEffect } from 'react';
-import axios from 'axios';
+import MovieDisplay from './components/MovieDisplay';
 
 function App() {
-  const [movieData, setMovieData] = useState([]);
-
-  useEffect(() => {
-    getMovieData();
-  }, [])
-
-  const getMovieData = async () => {
-    console.log("run once");
-    const results = await axios.get("https://ghibliapi.herokuapp.com/films");
-    console.log("response: ", results);
-    setMovieData(results.data);
-  }
-
   return (
     <div className="App">
-      <h1>Ghibli Movie</h1>
-      <div className="data-ui"><h3>data: {movieData.length > 0 ? movieData[0].title : "waiting for data..."}</h3></div>
+      <MovieDisplay />
     </div>
   );
 }
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function MovieDisplay() {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    getMovieData();
+  }, [])
+
+  const getMovieData = async () => {
+    console.log("run once");
+    const results = await axios.get("https://ghibliapi.herokuapp.com/films");
+    console.log("response: ", results);
+    setMovieData(results.data);
+  }
+
+  return (
+    <div className="MovieDisplay">
+      <h1>Ghibli Movie:</h1>
+      <div className="data-ui"><h3>{movieData.length > 0 ? movieData[0].title : "waiting for data..."}</h3></div>
+    </div>
+  );
+}
+
+export default MovieDisplay;
